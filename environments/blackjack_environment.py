d01ed73e69cc87d8a62dc6728dd8cf5f26f41afc
smstojanovic <stojanovich.stevan@live.com>
2020-05-07 20:04:19 +1000

    finalising structures, environments and tests

--- environments/blackjack_environment.py
+++ environments/blackjack_environment.py
@@ -1,6 +1,12 @@
 from environments.env import Env
 from games.cards.blackjack.game import BlackJackGame, Action
+from enum import Enum
 import numpy as np
+
+class PlayerType(Enum):
+    Player = 'Player'
+    Dealer = 'Dealer'
+
 
 class BlackJackEnv(Env):
     def __init__(self, num_decks = 6):
@@ -21,22 +27,28 @@ class BlackJackEnv(Env):
         self.game.render()
 
     def decode(self):
-        dealer_decode = self.player_decode('dealer')
-        player_decode = self.player_decode('player')
+        dealer_decode = self.player_decode(PlayerType.Player)
+        player_decode = self.player_decode(PlayerType.Dealer)
 
-        return dealer_decode - 1 + player_decode*(self.max_val + 1)
+        return dealer_decode + player_decode*self.dealer.critical_value
 
     def state_bounds(self):
         min = 0 # TODO: make this programatic
-        max = (22 - 1 - 1) + (22 - 4)*(self.max_val + 1) # TODO: make this programatic
+        max = (self.dealer.critical_value - 2) + (self.player.critical_value - 4)*(self.dealer.critical_value) # TODO: make this programatic
 
         return min, max
 
+    def get_player_min_value(self, player_type):
+        if player_type == PlayerType.Player:
+            return 4
+        return 2
+
     def player_decode(self, player_type):
-        sub = 1
-        if player_type == 'player':
+        sub = self.get_player_min_value(PlayerType.Dealer)
+
+        if player_type == PlayerType.Player:
             mv = self.game.player.get_max_value()
-            sub = 4
+            sub = self.get_player_min_value(PlayerType.Player)
         else:
             mv = self.game.dealer.get_max_value()
 
---
+++ games/cards/blackjack/pytest/test_game.py
@@ -0,0 +1,75 @@
+import os
+print(os.getcwd())
+
+import pytest
+from games.cards.blackjack.game import BlackJackGame, Action
+from games.cards.blackjack.bj_cards import BJCard, CustomBJDeck, StandardBJDeck
+
+def test_dealer_push():
+    # dealer push test 2
+    bjgame = BlackJackGame(verbose = False)
+
+    cards = [
+        BJCard('King','Spade'),
+        BJCard('King','Heart'),
+        BJCard('Queen','Heart'),
+        BJCard('6','Heart'),
+        BJCard('6','Spade')
+    ]
+
+    cards.reverse()
+    cdeck = CustomBJDeck(cards, False)
+    bjgame.set_deck(cdeck, False)
+
+    done, reward = bjgame.take_action(Action.Stay)
+
+    bjgame.dealer.get_max_value()
+    #bjgame.dealer.render()
+
+    assert done and reward == 0 and bjgame.dealer.get_max_value() == 22
+
+def test_dealer_continue():
+    bjgame = BlackJackGame(verbose = False)
+
+    cards = [
+        BJCard('Ace','Spade'),
+        BJCard('King','Spade'),
+        BJCard('King','Heart'),
+        BJCard('6','Heart'),
+        BJCard('5','Spade'),
+        BJCard('5','Diamond')
+    ]
+
+    cards.reverse()
+    cdeck = CustomBJDeck(cards, False)
+    bjgame.set_deck(cdeck, False)
+
+    done, reward = bjgame.take_action(Action.Stay)
+
+    bjgame.dealer.get_max_value()
+    #bjgame.dealer.render()
+
+    assert done and reward == 1 and bjgame.dealer.get_max_value() == 17
+
+def test_dealer_bust():
+    # dealer bust test
+    bjgame = BlackJackGame(verbose = False)
+
+    cards = [
+        BJCard('King','Spade'),
+        BJCard('King','Heart'),
+        BJCard('Queen','Heart'),
+        BJCard('6','Heart'),
+        BJCard('7','Spade')
+    ]
+
+    cards.reverse()
+    cdeck = CustomBJDeck(cards, False)
+    bjgame.set_deck(cdeck, False)
+
+    done, reward = bjgame.take_action(Action.Stay)
+
+    bjgame.dealer.get_max_value()
+    #bjgame.dealer.render()
+
+    assert done and reward == 1 and bjgame.dealer.get_max_value() == 23
